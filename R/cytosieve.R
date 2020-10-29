6e0a617a0fb40e84e74f940a9fe53abe70ccb80b
Colby Ford <colbytylerford@gmail.com>
2020-10-29 14:24:01 -0400

    Add initial read filtering capability

---
+++ R/cytosieve.R
@@ -0,0 +1,20 @@
+#'############################
+#' @title cytosieve
+#'
+#' @description cytosieve provides functionality for in silico cytometric filtering of bulk sequencing data with transcriptional profiles.
+#' @keywords genomics phylogenetics cytology bioinformatics biology
+#'
+#' @docType package
+#' @name cytosieve-package
+#' @aliases cytosieve cytosieve-package
+#'
+#' @author Colby T. Ford, \email{colby.ford@@uncc.edu} \url{https://orcid.org/0000-0002-7859-3622}
+#'
+#' @import ShortRead
+#' @import Biostrings
+#' @import dplyr
+#' @import stringr
+#'
+#' @seealso \url{https://github.com/colbyford/cytosieve}
+
+NULL
---
+++ test/test_filtering.R
@@ -0,0 +1,29 @@
+library(cytosieve)
+library(Biostrings)
+library(stringr)
+
+## Genes that are NOT expressed in Schizonts
+genes <- readDNAStringSet("data/Pvivax_schizont_filter_all29.fasta")[1:5]
+
+# samples <- read.table("sample_summary.txt", header = TRUE, stringsAsFactors = FALSE)
+# input_R1_fastq_path <- samples$FileR1[sample]
+# output_R1_fastq_path <- paste0(str_remove(input_R1_fastq_path, ".fastq"), "_filtered.fastq")
+# input_R2_fastq_path <- samples$FileR2[sample]
+# output_R2_fastq_path <- paste0(str_remove(input_R2_fastq_path, ".fastq"), "_filtered.fastq")
+
+## An example paired set of P. vivax samples
+input_R1_fastq_path <- "data/Pvivax_sample_r1_1000reads.fastq"
+output_R1_fastq_path <- paste0(str_remove(input_R1_fastq_path, ".fastq"), "_filtered.fastq")
+input_R2_fastq_path <- "data/Pvivax_sample_r2_1000reads.fastq"
+output_R2_fastq_path <- paste0(str_remove(input_R2_fastq_path, ".fastq"), "_filtered.fastq")
+
+# strsplit(basename(input_R1_fastq_path), split="\\.")[[1]][2]
+
+filter_reads(input_path = input_R1_fastq_path,
+             output_path = output_R1_fastq_path,
+             genes_to_find = genes,
+             eliminate_matches = TRUE,
+             pct_variability = 0.10,
+             paired = TRUE,
+             input_r2_path = input_R2_fastq_path,
+             output_r2_path = output_R2_fastq_path)
